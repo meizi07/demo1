812ceb973602c90f573ca410c2ecff0cfee45dfe
Chinyi Pam <chinyi.pam@gmail.com>
2023-11-28 17:54:00 +0800

    建立 NewTodoModal markup

--- src/components/modals/todos/NewTodoModal.vue
+++ src/components/modals/todos/NewTodoModal.vue
@@ -6,7 +6,7 @@
     tabindex="-1"
     aria-hidden="true"
   >
-    <div class="modal-dialog modal-dialog-centered mw-650px">
+    <div class="modal-dialog modal-dialog-centered mw-500px">
       <div class="modal-content rounded">
         <div class="modal-header pb-0 border-0 justify-content-end">
           <div
@@ -19,6 +19,52 @@
 
         <div class="modal-body scroll-y px-10 px-lg-15 pt-0 pb-15">
           modal content
+          <el-form
+            id="kt_modal_new_target_form"
+            @submit.prevent="submit()"
+            :model="targetData"
+            :rules="rules"
+            ref="formRef"
+            class="form"
+          >
+            <el-form-item label="事項">
+              <el-input
+                v-model="targetData.title"
+                name="title"
+                placeholder="事項"
+              />
+            </el-form-item>
+
+            <el-form-item label="說明">
+              <el-input
+                v-model="targetData.description"
+                type="textarea"
+                name="description"
+                placeholder="說明"
+              />
+            </el-form-item>
+
+            <el-form-item label="完成期限">
+              <el-date-picker
+                v-model="targetData.dueDate"
+                type="date"
+                name="dueDate"
+                placeholder="請選擇完成期限"
+                style="width: 100%"
+              />
+            </el-form-item>
+
+            <el-form-item label="指定案件">
+              <el-select
+                v-model="targetData.project"
+                placeholder="請指定案件"
+                name="project"
+              >
+                <el-option label="Zone one" value="shanghai" />
+                <el-option label="Zone two" value="beijing" />
+              </el-select>
+            </el-form-item>
+          </el-form>
         </div>
       </div>
     </div>
@@ -28,5 +74,22 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
+interface NewTodoData {
+  title: string;
+  description: string;
+  dueDate: string;
+  project: string;
+}
+
 const newTodoModalRef = ref<HTMLElement | null>(null);
+const formRef = ref<HTMLFormElement | null>(null);
+const targetData = ref<NewTodoData>({
+  title: "",
+  description: "",
+  dueDate: "",
+  project: "",
+});
+const rules = ref({});
+
+// TODO:取得所有案件名稱
 </script>
