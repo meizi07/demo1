f44d0ba72fcc35ca2855e49d9e2a7f2c72a7db74
Chinyi Pam <chinyi.pam@gmail.com>
2023-11-30 15:09:05 +0800

    新增 todo store & fetchTodoData

---
+++ src/stores/todo.ts
@@ -0,0 +1,78 @@
+import { ref, type Ref } from "vue";
+import type { AxiosResponse } from "axios";
+import ApiService from "@/core/services/ApiService";
+import { defineStore } from "pinia";
+import { useAuthStore } from "@/stores/auth";
+
+interface Todo {
+  UUID: string;
+  ProjectName: string;
+  Item: string;
+  Description: string;
+  Deadline: string;
+  Account: string;
+  Status: string;
+}
+
+enum TodoStatus {
+  Unfinished = "0",
+  Finished = "1",
+}
+
+export const useTodoStore = defineStore("todo", () => {
+  const authStore = useAuthStore();
+  const unfinishedData = ref<Todo[]>([]);
+  const finishedData = ref<Todo[]>([]);
+
+  function _createFormData() {
+    const formData = new FormData();
+
+    formData.append("orgId", authStore.user.orgId);
+    formData.append("account", authStore.user.account);
+    formData.append("token", authStore.user.token);
+
+    return formData;
+  }
+
+  function _generateApiService(isFinish: TodoStatus) {
+    const formData = _createFormData();
+
+    formData.append("isFinish", isFinish);
+
+    return ApiService.post("personal/getToDoListForPersonal", formData);
+  }
+
+  function _processTodoData(
+    response: AxiosResponse,
+    dataContainer: Ref<Todo[]>
+  ) {
+    if (response.data.success) {
+      dataContainer.value = response.data.success;
+    } else {
+      console.error(
+        "獲取待辦清單失敗，狀態： " +
+          response.status +
+          " " +
+          response.statusText
+      );
+
+      return [];
+    }
+  }
+
+  async function fetchTodoData() {
+    try {
+      const [unfinishedResponse, finishedResponse] = await Promise.all([
+        _generateApiService(TodoStatus.Unfinished),
+        _generateApiService(TodoStatus.Finished),
+      ]);
+
+      _processTodoData(unfinishedResponse, unfinishedData);
+      _processTodoData(finishedResponse, finishedData);
+    } catch (error) {
+      console.error("API 請求錯誤：", error);
+    }
+  }
+
+  return { finishedData, unfinishedData, fetchTodoData };
+});
