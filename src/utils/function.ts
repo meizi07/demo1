79de3259ed60fc7168662933de4c3ec17d21bee4
Chinyi Pam <chinyi.pam@gmail.com>
2023-12-11 11:31:11 +0800

    Util 新增 debounce function

--- src/utils/function.ts
+++ src/utils/function.ts
@@ -69,3 +69,20 @@ export function formatDate(dateString: string): string {
   const year = date.getFullYear().toString();
   return `${year}-${month}-${day}`;
 }
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
