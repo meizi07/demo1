4ce7f75b8043d40bbdd7c01628f3d1fdb8d67535
Chinyi Pam <chinyi.pam@gmail.com>
2023-12-13 16:26:33 +0800

    指定預設記錄日期和記錄者

--- src/components/housing/EditProjectInfo.vue
+++ src/components/housing/EditProjectInfo.vue
@@ -17,16 +17,24 @@
 
             <tr>
               <td class="text-muted">
-                <label class="d-flex align-items-center fs-6 fw-semobold mb-4">
+                <label class="d-flex align-items-center fs-6 fw-semobold">
                   紀錄日期
                 </label>
               </td>
-              <td class="fw-bold">{{ projectInfoData.RecordDate }}</td>
+              <td class="fw-bold">
+                <el-date-picker
+                  v-model="projectInfoData.RecordDate"
+                  type="date"
+                  placeholder="選擇紀錄日期"
+                  size="large"
+                  :default-value="new Date()"
+                />
+              </td>
             </tr>
 
             <tr>
               <td class="text-muted">
-                <label class="d-flex align-items-center fs-6 fw-semobold mb-4">
+                <label class="d-flex align-items-center fs-6 fw-semobold">
                   紀錄者
                 </label>
               </td>
@@ -62,6 +70,7 @@ import ApiService from "@/core/services/ApiService";
 import type { Account } from "@/types/Project";
 import type { ProjectInfo } from "@/types/Housing";
 
+const authStore = useAuthStore();
 const route = useRoute();
 
 const recorderOptions = ref<Account[]>([]);
@@ -71,9 +80,17 @@ const projectInfoData = ref<ProjectInfo>({
   RecordDate: moment().format("YYYY-MM-DD"),
 });
 
-async function fetchRecorderOptions() {
-  const authStore = useAuthStore();
+function handleDefaultRecorder() {
+  const defaultRecorder = recorderOptions.value.find(
+    (item) => item.AccountID.trim() == authStore.user.account.trim()
+  );
 
+  if (defaultRecorder) {
+    projectInfoData.value.Recorder = defaultRecorder.Name;
+  }
+}
+
+async function fetchRecorderOptions() {
   const formData = authStore.createAuthFormData({
     orgId: true,
     account: true,
@@ -87,6 +104,7 @@ async function fetchRecorderOptions() {
 
     if (response.data && response.data.success) {
       recorderOptions.value = response.data.success;
+      handleDefaultRecorder();
     } else {
       console.error(
         `獲取記錄者失敗，狀態： ${response.status} ${response.statusText}`
