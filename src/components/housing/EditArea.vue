365be991ca40d5ffce1c7796c4c2940206402ac5
Chinyi Pam <chinyi.pam@gmail.com>
2023-12-18 10:56:03 +0800

    調整 area table style

--- src/components/housing/EditArea.vue
+++ src/components/housing/EditArea.vue
@@ -2,28 +2,23 @@
   <div class="card">
     <div class="card-body d-flex flex-column p-9">
       <div class="table-responsive">
-        <div
-          id="DataTables_Table_0_wrapper"
-          class="dataTables_wrapper dt-bootstrap4 no-footer"
-        >
+        <div class="dataTables_wrapper dt-bootstrap4 no-footer">
           <div class="table-responsive">
             <table
               class="table align-middle table-row-bordered fs-6 gy-5 housing_info_table dataTable no-footer"
-              id="DataTables_Table_0"
-              style="width: 939px"
             >
               <thead>
                 <tr class="text-start text-gray-800 fw-bold fs-7 gs-0">
                   <th style="width: 15%">區域</th>
                   <th style="width: 35%">說明</th>
-                  <th style="width: 45%">照片</th>
-                  <th style="width: 5%"></th>
+                  <th style="width: 40%">照片</th>
+                  <th style="width: 10%"></th>
                 </tr>
               </thead>
 
               <tbody class="fw-semibold text-gray-800">
                 <tr v-for="(item, index) in areaData" :key="index">
-                  <td>
+                  <td class="pe-5 py-10">
                     <el-form-item>
                       <el-input
                         v-model="item.Area"
@@ -34,7 +29,7 @@
                     </el-form-item>
                   </td>
 
-                  <td>
+                  <td class="px-5 py-10">
                     <el-form-item>
                       <el-input
                         v-model="item.Description"
@@ -45,18 +40,27 @@
                     </el-form-item>
                   </td>
 
-                  <td>
+                  <td class="px-5 py-10">
                     <div
                       v-for="(img, imgIndex) in item.DetailRecord"
                       :key="imgIndex"
-                      class="d-flex mb-3 align-items-center gap-3"
+                      class="d-flex mb-6 align-items-center gap-4"
                     >
-                      <img
-                        :src="img.FileImage"
-                        class="w-125px h-125px object-fit-cover rounded"
-                      />
+                      <div class="position-relative">
+                        <img
+                          :src="img.FileImage"
+                          class="w-125px h-125px object-fit-cover rounded"
+                        />
+                        <button
+                          type="button"
+                          class="btn btn-icon btn-light btn-active-color-primary position-absolute top-0 start-100 w-25px h-25px translate-middle rounded-circle"
+                          @click="removeThumbnail(index, imgIndex)"
+                        >
+                          <i class="ki-duotone ki-minus fs-2"></i>
+                        </button>
+                      </div>
 
-                      <el-form-item class="mb-0">
+                      <el-form-item class="w-100 mb-0">
                         <el-input
                           v-model="img.Description"
                           type="text"
@@ -64,19 +68,12 @@
                           placeholder="圖片描述"
                         />
                       </el-form-item>
-
-                      <button
-                        type="button"
-                        class="btn btn-sm"
-                        @click="removeThumbnail(index, imgIndex)"
-                      >
-                        刪除
-                      </button>
                     </div>
 
                     <label
                       :for="`fileInput${index}`"
                       class="btn bg-light-secondary d-flex flex-column flex-center w-125px h-125px"
+                      style="border: 1px dashed rgb(220, 223, 230)"
                     >
                       <i class="ki-duotone ki-plus text-gray-300 fs-4x"></i>
                       <input
@@ -90,10 +87,10 @@
                     </label>
                   </td>
 
-                  <td>
+                  <td class="ps-5 py-10">
                     <button
                       type="button"
-                      class="d-block btn btn-sm btn-icon text-muted text-hover-danger"
+                      class="d-block m-auto btn btn-sm btn-icon text-muted text-hover-danger"
                       @click="deleteAreaRow(index)"
                     >
                       <i class="ki-duotone ki-cross fs-1">
@@ -109,7 +106,7 @@
         </div>
       </div>
 
-      <div class="d-flex justify-content-start mb-8">
+      <div class="d-flex justify-content-start mt-12 mb-8">
         <button
           type="button"
           class="btn btn-sm btn-light-primary"
