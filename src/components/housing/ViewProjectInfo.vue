27e142cb9742a5592f7fb415a655bef5a5f96d88
Chinyi Pam <chinyi.pam@gmail.com>
2023-12-19 17:27:57 +0800

    新增檢視案件紀錄

---
+++ src/components/housing/ViewProjectInfo.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card mb-5 mb-xl-10">
+    <div class="card-body d-flex flex-column p-9">
+      <div class="table-responsive">
+        <table
+          class="table align-middle table-row-bordered mb-0 fs-6 gy-5 table_half_col"
+        >
+          <tbody class="fw-semibold text-gray-800">
+            <tr>
+              <td class="text-muted">
+                <label class="d-flex align-items-center fs-6 fw-semobold">
+                  案件編號
+                </label>
+              </td>
+              <td class="fw-bold">{{ singleHousingData?.ProjectID }}</td>
+            </tr>
+
+            <tr>
+              <td class="text-muted">
+                <label class="d-flex align-items-center fs-6 fw-semobold">
+                  紀錄日期
+                </label>
+              </td>
+              <td class="fw-bold">
+                {{
+                  moment(singleHousingData?.RecordDate).format("YYYY-MM-DD") ||
+                  "-"
+                }}
+              </td>
+            </tr>
+
+            <tr>
+              <td class="text-muted">
+                <label class="d-flex align-items-center fs-6 fw-semobold">
+                  紀錄者
+                </label>
+              </td>
+              <td class="fw-bold">
+                {{ singleHousingData?.Recorder || "-" }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import moment from "moment";
+import { useHousingStore } from "@/stores/housing";
+
+const housingStore = useHousingStore();
+const { singleHousingData } = storeToRefs(housingStore);
+</script>
--- src/views/before-project/client-info/housing/HousingDetails.vue
+++ src/views/before-project/client-info/housing/HousingDetails.vue
@@ -1,19 +1,83 @@
 <template>
-  <h1>housing details page</h1>
-  <h2>projectID: {{ projectId }}</h2>
-  <br />
-  <pre>{{ singleHousingData }}</pre>
+  <div class="toolbtn container-xxl d-flex align-items-center gap-2 gap-lg-3">
+    <!-- 暫時隱藏 -->
+    <!-- <button type="submit" class="btn btn-sm fw-bold btn-success">送簽</button> -->
+
+    <router-link
+      :to="`/befort-project/client-info/housing-list`"
+      class="btn btn-sm fw-bold bg-body btn-color-gray-700 btn-active-color-primary"
+    >
+      編輯
+    </router-link>
+  </div>
+
+  <div class="d-flex flex-column gap-7 gap-lg-10">
+    <div class="d-flex flex-wrap flex-stack gap-5 gap-lg-10">
+      <div class="common_tab w-100 pt-3">
+        <ul
+          class="nav nav-pills nav-pills-custom row position-relative mx-0 mb-3"
+        >
+          <li class="nav-item p-0">
+            <a
+              class="nav-link d-flex justify-content-center w-100 border-0 h-100 active"
+              data-bs-toggle="tab"
+              role="tab"
+              href="#housing_init_record"
+            >
+              <span class="nav-text text-gray-800 fw-bold fs-6 mb-3">
+                屋況初始紀錄
+              </span>
+              <span
+                class="bullet-custom position-absolute z-index-2 bottom-0 w-100 h-4px"
+              ></span>
+            </a>
+          </li>
+
+          <li class="nav-item p-0">
+            <a
+              class="nav-link d-flex justify-content-center w-100 border-0 h-100"
+              data-bs-toggle="tab"
+              role="tab"
+              href="#housing_measuring_record"
+            >
+              <span class="nav-text text-gray-800 fw-bold fs-6 mb-3">
+                丈量紀錄
+              </span>
+              <span
+                class="bullet-custom position-absolute z-index-2 bottom-0 w-100 h-4px"
+              ></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="tab-content">
+      <div
+        class="tab-pane fade show active"
+        id="housing_init_record"
+        role="tabpanel"
+      >
+        <ViewProjectInfo />
+      </div>
+
+      <div
+        class="tab-pane fade show"
+        id="housing_measuring_record"
+        role="tabpanel"
+      ></div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from "vue-router";
-import { storeToRefs } from "pinia";
+import ViewProjectInfo from "@/components/housing/ViewProjectInfo.vue";
 import { useHousingStore } from "@/stores/housing";
 
 const route = useRoute();
 const projectId = route.params.projectId;
 const housingStore = useHousingStore();
-const { singleHousingData } = storeToRefs(housingStore);
 
 housingStore.fetchSingleHousingData(projectId as string);
 </script>
